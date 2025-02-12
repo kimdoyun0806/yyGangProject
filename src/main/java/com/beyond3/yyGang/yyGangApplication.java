7f145ee590bce9fc90bc91761b0613b132e9e120
kimdoyun0806 <gim_doyun@naver.com>
2025-02-12 22:40:11 +0900

    첫 commit 테스트

---
+++ src/main/java/com/beyond3/yyGang/yyGangApplication.java
@@ -0,0 +1,13 @@
+package com.beyond3.yyGang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class yyGangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(yyGangApplication.class, args);
+	}
+
+}
