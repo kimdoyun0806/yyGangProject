7924ebfd856622eb75c7ea7cf79a31b6b382288a
kimdoyun0806 <gim_doyun@naver.com>
2025-02-12 22:53:17 +0900

    두 번째 commit 테스트

--- src/main/java/com/beyond3/yyGang/ageCategory/gittest.java
+++ src/main/java/com/beyond3/yyGang/ageCategory/gittest.java
@@ -2,6 +2,6 @@ package com.beyond3.yyGang.ageCategory;
 
 public class gittest {
     public static void main(String[] args) {
-        System.out.println("깃헙 테스트");
+        System.out.println("깃헙 테스트");;
     }
 }
